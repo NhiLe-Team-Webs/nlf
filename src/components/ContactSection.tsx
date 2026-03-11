f76c616089aff2c003fa99447f51c693e7be69c5
Văn Đức Tân <vanductan.nlt@gmail.com>
2026-03-11 14:41:44 +0700

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -49,7 +49,7 @@ const ContactSection = () => {
                     {contact.address}
                   </h3>
                   <p className="text-slate-600 leading-relaxed">
-                    72 Nại Nam, Hoà Cường Bắc, Hải Châu, Đà Nẵng 50000, Vietnam
+                    74 Nại Nam, Hoà Cường Bắc, Hải Châu, Đà Nẵng 50000, Vietnam
                   </p>
                 </div>
               </div>
